ad2d9eba17773a9a77e936c03f0127b070657c6c
SageKyle <ominyipaulo@gmail.com>
2022-11-12 20:48:30 +0100

    uploaded files
    - completed mobile layout

---
+++ app.js
@@ -0,0 +1,31 @@
+// UI Variables
+const adviceID = document.querySelector('.id');
+const adviceContainer = document.querySelector('.advice');
+const toggleAdvice = document.querySelector('.get-advice');
+
+// fetch advice
+const getAdvice = async () => {
+	// initialize variable
+	let advice;
+	// request for advice
+	await fetch('https://api.adviceslip.com/advice')
+		.then((res) => res.json())
+		.then((data) => (advice = data.slip))
+		.catch((err) => console.log(err));
+
+	// display advice
+	// advice ID
+	adviceID.textContent = advice.id;
+	// advice
+	adviceContainer.textContent = '"' + advice.advice + '"';
+};
+
+// show an advice on initial loading
+document.addEventListener('DOMContentLoaded', () => {
+	getAdvice();
+});
+
+// show another advice
+toggleAdvice.addEventListener('click', () => {
+	getAdvice();
+});
